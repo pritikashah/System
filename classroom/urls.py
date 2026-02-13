87ab34d422e1ae78badeee6df901d39e1eb05757
TirthaPatel978 <ivory32tirthap@gmail.com>
2026-02-13 23:17:08 +0530

    Delete classroom directory

--- classroom/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
-    path('teacher/dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
-]
