0e208d899b994e904d9dce80d5d9ffa2438b1755
TirthaPatel978 <ivory32tirthap@gmail.com>
2026-02-17 23:50:23 +0530

    Code based student enrollment

--- courses/models.py
+++ courses/models.py
@@ -4,17 +4,21 @@ from django.conf import settings
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    course_code = models.CharField(max_length=20)
+
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         limit_choices_to={'user_type': 'teacher'}
     )
+
     students = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         related_name='enrolled_courses',
         blank=True,
         limit_choices_to={'user_type': 'student'}
     )
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
