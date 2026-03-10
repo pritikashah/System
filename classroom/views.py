35c6ccbac199ed81f86591c780949a438551974e
pritikashah <shahpritika19@gmail.com>
2026-03-10 17:16:09 +0530

    Delete classroom directory

--- classroom/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def student_dashboard(request):
-    return render(request, 'classroom/student_dashboard.html')
-
-def teacher_dashboard(request):
-    return render(request, 'classroom/teacher_dashboard.html')
